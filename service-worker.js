249e0159fe9159460cfef83c88689478f6acf5db
andychiu0913 <andychiu0913@gmail.com>
2026-04-01 23:47:58 +0800

    Enhance: 升級為 PWA 可安裝 App (含圖示與離線支援)

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'trading-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './calculator.html',
+    './script.js',
+    './style.css',
+    './icon.png',
+    './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
